bdf437b917ae47b4badebd242eaace1601b28aa0
josevegara04 <jbvegar@eafit.edu.co>
2026-04-15 13:37:58 -0500

    fix: fix imports

--- src/domain/repositories.py
+++ src/domain/repositories.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
-from .entities import Product, ChatMessage
+from src.domain.entities import Product, ChatMessage
 
 class IProductRepository(ABC):
     """
